d9d4c9f1de00dff9f7eb5251827735380f737011
Andrew <goodman.drew1@gmail.com>
2024-04-01 15:40:27 -0500

    navbar dropdown populating w topics dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
 
 import { getAllPosts } from "./services/getAllPosts.js";
+import { getAllTopics } from "./services/getAllTopics.js";
 
-import { Navbar } from "react-bootstrap";
+import { NavDropdown, Navbar } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import { Card } from "react-bootstrap";
@@ -16,11 +17,15 @@ import Row from "react-bootstrap/Row";
 
 export const App = () => {
   const [allPosts, setAllPosts] = useState([]);
+  const [allTopics, setAllTopics] = useState([]);
 
   //get employees from DB and set to employees state var
   useEffect(() => {
     getAllPosts().then((postsArray) => {
       setAllPosts(postsArray);
+    });
+    getAllTopics().then((topicsArray) => {
+      setAllTopics(topicsArray);
     });
   }, []);
 
@@ -30,7 +35,14 @@ export const App = () => {
         <Container>
           <Navbar.Brand href="#home">Learning Moments</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">All Posts</Nav.Link>
+            <NavDropdown title="All Posts" id="basic-nav-dropdown">
+              {allTopics.map((topicObj) => (
+                <NavDropdown.Item href={`#${topicObj.id}`} key={topicObj.id}>
+                  {topicObj.name}
+                </NavDropdown.Item>
+              ))}
+            </NavDropdown>
+
             <Nav.Link href="#my-posts">My Posts</Nav.Link>
             <Nav.Link href="#favorites">Favorites</Nav.Link>
             <Nav.Link href="#new-post">New Post</Nav.Link>
@@ -42,11 +54,12 @@ export const App = () => {
       <Container fluid>
         <Row>
           {allPosts.map((postObj) => {
-            {
-              console.log(postObj);
-            }
             return (
-              <Card className="text-center" style={{ width: "12rem" }}>
+              <Card
+                className="text-center"
+                style={{ width: "12rem" }}
+                key={postObj.id}
+              >
                 <Card.Body>
                   <Card.Title>{postObj.title}</Card.Title>
                   <Card.Text>{postObj.topic.name}</Card.Text>
